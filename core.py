0c8a4829409e8b1ed1d3261863588126bd35e273
Karl Hegci <karlhegci@inbox.lv>
2026-08-17 19:14:53 +0300

    feat:auto clicker functionality implementation

--- core.py
+++ core.py
@@ -1,37 +1,25 @@
 import time
-import threading
+import pyautogui
 
 class AutoClicker:
-    def __init__(self, interval=0.1):
-        self.interval = interval  # Time between clicks
-        self.running = False  # Flag to control the clicking loop
-        self.thread = None  # Thread for running the clicker
-
-    def start(self):
-        if not self.running:
-            self.running = True
-            self.thread = threading.Thread(target=self._click_loop)
-            self.thread.start()
-
-    def stop(self):
+    def __init__(self, interval: float):
+        self.interval = interval
         self.running = False
-        if self.thread is not None:
-            self.thread.join()  # Wait for clicker thread to finish
 
-    def _click_loop(self):
-        while self.running:
-            self._click()
-            time.sleep(self.interval)
+    def start_clicking(self):
+        self.running = True
+        print('AutoClicker started. Press Ctrl+C to stop.')
+        try:
+            while self.running:
+                pyautogui.click()
+                time.sleep(self.interval)
+        except KeyboardInterrupt:
+            self.stop_clicking()
 
-    @staticmethod
-    def _click():
-        # Here we would implement the actual click event. For example:
-        # pyautogui.click()
-        print("Click!")  # Simulated click event
+    def stop_clicking(self):
+        self.running = False
+        print('AutoClicker stopped.')
 
 if __name__ == '__main__':
-    clicker = AutoClicker(interval=0.5)
-    clicker.start()
-    time.sleep(2)  # Click for 2 seconds
-    clicker.stop()
-    print("AutoClicker stopped.")
+    clicker = AutoClicker(interval=0.1)  # Adjust interval as needed
+    clicker.start_clicking()
